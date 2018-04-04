292de2c92dc6d8a178f67a3c4b8481eab5f8c78a
Behtsa <behtsabet14@gmail.com>
2018-04-03 21:43:22 -0500

    consumed Manuel Orozco's API

--- bundle.js
+++ bundle.js
@@ -12,7 +12,8 @@ var rp = require('request-promise');
 // var cheerio = require('cheerio'); // Basically jQuery for node.js
 
 var options = {
-    uri: 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5a577871909255973d40829c2fc8d31f/19.4844699,-99.05361769999999',
+    // uri: 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/5a577871909255973d40829c2fc8d31f/19.4844699,-99.05361769999999',
+    uri: 'http://examen-laboratoria-sprint-5.herokuapp.com/topics',
     headers: {
         'User-Agent': 'Request-Promise'
     },
